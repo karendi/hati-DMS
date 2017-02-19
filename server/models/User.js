b4e511c9c0eba46eacaec34fa9ad04e1bdee0f97
Jackie W. Macharia <jackline.macharia@andela.com>
2017-02-19 14:33:38 +0300

    [Feature #140012123] Set up User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,67 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    title: DataTypes.STRING
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    fName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        msg: 'Must be a valid email address'
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: {
+            args: 3,
+            msg: "Username must be atleast 3 characters long"
+          }
+        }
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+          args: 3
+          }
+        }
+      }
+    }
   }, {
     classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+      associate (models) => {
+        User.hasMany(models.Document, {
+          foreignKey: 'userId',
+          as: 'documents'
+        });
+        User.belongsTo(models.Role, {
+          foreignKey: 'roleId'
+        });
+      }
+    },
+    hooks: {
+      beforeCreate(user) {
+        user.password = bcrypt.hashSync(user.password);
+      },
+      beforeUpdate(user) {
+        user.password = bcrypt.hashSync(user.password);
       }
     }
   });
