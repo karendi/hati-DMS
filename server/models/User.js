a128b126d2ca55cb8bd3c7c99fd647444f179571
Jackie W. Macharia <jackline.macharia@andela.com>
2017-02-20 16:07:18 +0300

    [Feature #140012123] Include password hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcryptjs');
+import Bcrypt from 'bcryptjs';
+const salt = Bcrypt.genSaltSync(10);
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -29,14 +30,25 @@ module.exports = (sequelize, DataTypes) => {
           }
         }
       },
-      password: {
+      password_hash: {
         type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
         allowNull: false,
         validate: {
           len: {
-          args: 3
+          args: [6, 200],
+          msg: 'Password should be atleast 6 characters long'
           }
-        }
+        },
+        set: (value) => {
+          hashedPassword = Bcrypt.hashSync(value, salt);
+          this.setDataValue('password', value);
+          this.setDataValue('salt', salt);
+          this.setDataValue('password_hash', hashedPassword);
+        },
       }
     }
   }, {
@@ -53,10 +65,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     hooks: {
       beforeCreate(user) {
-        user.password = bcrypt.hashSync(user.password);
+        user.password = Bcrypt.hashSync(user.password);
       },
       beforeUpdate(user) {
-        user.password = bcrypt.hashSync(user.password);
+        user.password = Bcrypt.hashSync(user.password);
       }
     }
   });
