f883be482c60165fe1b907a24ddd5528a186613f
Jackie W. Macharia <jackline.macharia@andela.com>
2017-02-22 16:16:42 +0300

    [Chore #140359795] Set up user routes

---
+++ server/routes/UserRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import UserController from './controllers/UserController';
+import Authentication from './middleware/Auth';
+
+const userRouter = express.Router;
+
+userRouter.route('/')
+  .get(Authentication.verifyUser, Authentication.verifyAdmin, UserController.listAllUsers)
+  .post(UserController.createNewUser);
+
+userRouter.route('/login')
+  .post(UserController.loginUser);
+
+userRouter.route('/logout')
+  .post(Authentication.verifyUser, Authentication.logout, UserController.logoutUser);
+
+userRouter.route('/:id')
+  .get(Authentication.verifyUser, UserController.findUserById)
+  .put(Authentication.verifyUser, UserController.updateUser)
+  .delete(Authentication.verifyUser, Authentication.verifyAdmin, UserController.deleteUser);
+
+userRouter.route('/:id/documents')
+  .get(Authentication.verifyUser, UserController.listUserDocuments);
+
+export default userRouter;
