591d04ed34ed13d239d064595de1f4dff2a58552
Jackie W. Macharia <jackline.macharia@andela.com>
2017-02-22 16:13:49 +0300

    [Chore #140359795] Refactor server.js and move file to root folder

---
+++ server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import app from './server/routes/index';
+
+dotenv.config({
+  silent: true
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (error) => {
+	if (!error) {
+		console.log(`App listening on port ${port}...`);
+	} else {
+		console.log('error');
+	}
+});
