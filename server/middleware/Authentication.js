bcbb0c86a553eb158390e0b2f023b9eafeda3c19
Jackie W. Macharia <jackline.macharia@andela.com>
2017-02-25 00:22:28 +0300

    [Feature #140012123] Troubleshoot user authentication

--- server/middleware/Authentication.js
+++ server/middleware/Authentication.js
@@ -1,40 +1,45 @@
+import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import db from '../models';
+
+dotenv.config({ silent: true });
 
 const secret = process.env.SECRET || 'wearethepirateswhodontdoanything';
 
 class Authentication {
   static verifyUser(req, res, next) {
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if (token) {
-      jwt.verify(token, secret, (err, decoded) => {
-        if (err) {
-          res.status(401).send({ message: 'Invalid token' });
-        } else {
-          req.decodedToken = decoded;
-          next();
-        }
-      });
-    } else {
-      res.status(401).send({ message: 'Verification failed' });
+    const token = req.body.token || req.query.token || req.headers.authorization || req.headers['x-access-token'];
+    if (!token) {
+      return res.status(401).send({ message: 'Verification failed' });
     }
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        res.status(401).send({ message: 'Invalid token' });
+      } else {
+        req.decodedToken = decoded;
+        next();
+      }
+      });
   }
 
   static verifyAdmin(req, res, next) {
     db.Role
       .findById(req.decodedToken.roleId)
       .then((role) => {
-        if (role.title === 'admin') {
+        if (role.title === 'admin' || role.id === 1) {
           next();
-        } else { return res.status(403).send({ message: 'Permission denied, admin only' }); }
+        } else {
+          return res.status(403).send({ message: 'Permission denied, admin only' });
+        }
       });
   }
 
   static logout(req, res, next) {
-    const token = req.headers.token || req.headers['x-access-token'];
+    const token = req.headers.token || req.headers.authorization || req.headers['x-access-token'];
     const decoded = req.decodedToken;
     if (token && decoded) {
       delete req.headers.token;
+      delete req.headers.authorization;
       delete req.headers['x-access-token'];
       delete req.decodedToken;
       next();
