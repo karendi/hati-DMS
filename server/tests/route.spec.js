6f5ecb0c35ba97b8db86fc6acbcb2818bb2a0c16
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-07 12:21:59 +0300

    [Feature #140993469] Write index route test

---
+++ server/tests/route.spec.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../routes/Index';
+
+dotenv.config({ silent: true });
+
+process.env.NODE_ENV = 'test';
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/GET Index Route', () => {
+  it('should return the welcome message', (done) => {
+    chai.request(app)
+    .get('/')
+    .end((err, res) => {
+      res.should.have.status(200);
+      res.body.message.should.equal('Welcome to Hati!');
+      done();
+    });
+  });
+});
