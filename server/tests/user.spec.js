74dd128cadab7434d065cf81563affb64d533e41
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-15 15:47:42 +0300

    [Feature #140993469] Add user authentication, deletion, logout and updating tests

--- server/tests/user.spec.js
+++ server/tests/user.spec.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = 'test';
 const should = chai.should();
 const roles = seeds.legitRoles;
 const users = seeds.legitUsers;
-const invalidUsers = seeds.invalidUsers;
+const invalidUser = seeds.invalidUsers;
 const admin = {};
 const user = {};
 
@@ -21,11 +21,9 @@ chai.use(chaiHttp);
  */
 
 describe('User API Spec', () => {
-
   let adminUserToken;
   let regUserToken;
   before((done) => {
-
     chai.request(app)
     .post('/users/login')
       .send({
@@ -35,7 +33,6 @@ describe('User API Spec', () => {
       .end((error, response) => {
         adminUserToken = response.body.token;
       });
-
     chai.request(app)
     .post('/users/login')
       .send({
@@ -48,20 +45,207 @@ describe('User API Spec', () => {
       });
   });
 
-   describe('Users Information' , () => {
+  describe('User Authentication' , () => {
+   it('should signup users successfully and set a token', (done) => {
+     chai.request(app)
+      .post('/users')
+        .send({
+          fName: 'See',
+          lName: 'Meeee',
+          email: 'imnew@example.com',
+          username: 'newestone',
+          password: 'hardquestions'
+        })
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.message.should.equal('User was successfully created');
+        res.body.data.username.should.equal('newestone');
+        should.exist(res.body.token);
+        done();
+      });
+   });
+   it('should not signup users if required fields are missing', (done) => {
+     chai.request(app)
+      .post('/users')
+        .send(invalidUser[1])
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.message.should.equal('Fill the required fields');
+        done();
+      });
+   });
+   it('should not sign up a user with an already existing email or username', (done) => {
+     chai.request(app)
+      .post('/users')
+        .send(users[2])
+      .end((err, res) => {
+        res.should.have.status(409);
+        res.body.message.should.equal('Email or username already exists');
+        done();
+      });
+   });
+   it('should return a token on signin', (done) => {
+     chai.request(app)
+      .post('/users/login')
+        .send({
+          email: users[2].email,
+          password: users[2].password
+        })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.message.should.equal('You were successfully logged in');
+        should.exist(res.body.token);
+        done();
+      });
+   });
+   it('should not sign in a user if required fields are missing', (done) => {
+     chai.request(app)
+      .post('/users/login')
+        .send({
+          email: users[2].email
+        })
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.message.should.equal('Invalid login credentials');
+        done();
+      });
+   });
+   it('should not sign in a non-registered user', (done) => {
+     chai.request(app)
+      .post('/users/login')
+        .send({
+          email: 'notthere@example.com',
+          password: 'hastalavista'
+        })
+      .end((err, res) => {
+        res.should.have.status(403);
+        res.body.message.should.equal('No user was found');
+        done();
+      });
+   });
+   it('should not sign in a user if password is invalid', (done) => {
+     chai.request(app)
+      .post('/users/login')
+        .send({
+          email: users[2].email,
+          password: 'ninitena'
+        })
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.message.should.equal('Invalid password');
+        done();
+      });
+   });
+ });
 
-     it('GET /users => should get the admin list of all users', (done) => {
-       chai.request(app)
-        .get('/users')
+ describe('Get Users' , () => {
+   it('should get a list of all users', (done) => {
+     chai.request(app)
+      .get('/users')
+      .set('authorization', regUserToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.message.should.equal('Listing available users');
+        done();
+      });
+    });
+    it('should require a token before listing available users', (done) => {
+     chai.request(app)
+      .get('/users')
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.message.should.equal('Verification failed');
+        done();
+      });
+    });
+    it('should not allow access to the list of users if the token is invalid', (done) => {
+     chai.request(app)
+      .get('/users')
+      .set('authorization', 'hdfhf743u43brf97dhewhurvgy382hch')
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.message.should.equal('Invalid token');
+        done();
+      });
+    });
+    it('should search for a user by id', (done) => {
+     chai.request(app)
+      .get('/users/4')
+      .set('authorization', regUserToken)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.message.should.equal('User found!');
+        should.exist(res.body.data);
+        done();
+      });
+    });
+    it('should return an error message if the user was not found', (done) => {
+     chai.request(app)
+      .get('/users/4098')
+      .set('authorization', regUserToken)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.message.should.equal('The user was not found');
+        done();
+      });
+    });
+  });
+
+  describe('User Search', () => {
+  });
+
+  describe('User Documents', () => {
+  });
+
+  describe('User Updating', () => {
+    it('should allow a user to update their data', (done) => {
+      chai.request(app)
+        .put('/users/2')
+        .set('authorization', regUserToken)
+        .send({
+          email: 'jk@example.com'
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.equal('User updated successfully');
+          should.exist(res.body.data);
+          done();
+        })
+    });
+  });
+
+  describe('User Deletion', () => {
+    it('should allow an admin to delete a user\'s account', (done) => {
+      chai.request(app)
+        .delete('/users/5')
         .set('authorization', adminUserToken)
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.message.should.equal('Listing available users');
+          res.body.message.should.equal('User was deleted successfully');
           done();
-        });
-     });
-   });
+        })
+    });
+  });
 
-
-
+  describe('User Logout', () => {
+    it('should log a user out', (done) => {
+      chai.request(app)
+        .post('/users/logout')
+        .set('authorization', regUserToken)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.equal('You were logged out successfully');
+          done();
+        })
+    });
+    it('should require a user to have a valid token to be logged out', (done) => {
+      chai.request(app)
+        .post('/users/logout')
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.message.should.equal('Verification failed');
+          done();
+        })
+    });
+  });
 });
