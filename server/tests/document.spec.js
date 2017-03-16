937eb68248b8483876fa64f783abdc7215990404
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-16 12:34:04 +0300

    [Feature #140993469] Add document create, update, delete and update tests

--- server/tests/document.spec.js
+++ server/tests/document.spec.js
@@ -1,23 +1,16 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import DocumentController from '../controllers/DocumentController'
 import app from '../routes/Index';
 import seeds from '../db/seeds/Index';
-import db from '../models/Index'
 
 process.env.NODE_ENV = 'test';
 
 const should = chai.should();
-const roles = seeds.legitRoles;
 const users = seeds.legitUsers;
 const docs = seeds.legitDocs;
-const admin = {};
-const user = {};
-
 chai.use(chaiHttp);
 
 describe('Document API Spec', () => {
-
   let adminUserToken;
   let regUserToken;
   before((done) => {
@@ -42,19 +35,133 @@ describe('Document API Spec', () => {
       });
   });
 
-   describe('Admin' , () => {
-     it('POST => should be able to list all available documents', (done) => {
-       chai.request(app)
-       .get('/documents')
+  describe('Create Documents', () => {
+    it('should allow a user to create a document', (done) => {
+      chai.request(app)
+        .post('/documents')
+        .set('authorization', regUserToken)
+        .send(docs[0])
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.message.should.equal('Document created successfully');
+          done();
+        });
+    });
+    it('should prevent a non-logged in user from creating documents', (done) => {
+      chai.request(app)
+        .post('/documents')
+        .send(docs[0])
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.message.should.equal('Verification failed');
+          done();
+        });
+    });
+    it('should ensure the title field is not blank', (done) => {
+      chai.request(app)
+        .post('/documents')
         .set('authorization', adminUserToken)
+        .send({
+          title: '',
+          content: docs[2].content
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.message.should.equal('Title field cannot be blank');
+          done();
+        });
+    });
+    it('should ensure the content field is not blank', (done) => {
+      chai.request(app)
+        .post('/documents')
+        .set('authorization', adminUserToken)
+        .send({
+          title: docs[2].title,
+          content: '',
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.message.should.equal('Content field cannot be blank');
+          done();
+        });
+    });
+  });
+
+  describe('Update Documents', () => {
+    it('should allow the owner of the document to update it', (done) => {
+      chai.request(app)
+        .put('/documents/6')
+        .set('authorization', regUserToken)
+        .send({
+          title: 'Grab Some Books Already!',
+          content: '',
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.equal('The document was updated successfully');
+          should.exist(res.body.data);
+          done();
+        });
+    });
+    it('should not allow updating if both title and content fields are blank', (done) => {
+      chai.request(app)
+        .put('/documents/6')
+        .set('authorization', regUserToken)
+        .send({
+          title: '',
+          content: '',
+        })
+        .end((err, res) => {
+          res.should.have.status(406);
+          res.body.message.should.equal('No update detected');
+          done();
+        });
+    });
+  });
+
+  describe('Delete Documents', () => {
+    it('should allow the document owner to delete their document', (done) => {
+      chai.request(app)
+        .delete('/documents/6')
+        .set('authorization', regUserToken)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.equal('The document was deleted successfully');
+          done();
+        });
+    });
+    it('should not allow non-logged in users to delete documents', (done) => {
+      chai.request(app)
+        .delete('/documents/5')
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.message.should.equal('Verification failed');
+          done();
+        });
+    });
+  });
+
+  describe('View Documents', () => {
+    it('should allow a user to list all available documents', (done) => {
+      chai.request(app)
+        .get('/documents')
+        .set('authorization', regUserToken)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.message.should.equal('Listing all documents');
           done();
         });
-     })
-   });
-
-
-
+    });
+    // it('should only list public documents', (done) => {
+    //   chai.request(app)
+    //     .get('/documents')
+    //     .set('authorization', regUserToken)
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.message.should.equal('Listing all documents');
+    //       should.not.exist(res.body.data.docs[2]);
+    //       done();
+    //     });
+    // });
+  });
 });
