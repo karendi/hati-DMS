cdead4966c2af249b291f5242bcb02a040902a73
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-16 18:28:49 +0300

    [Feature #141844919] Add pagination options in list all users

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -128,7 +128,8 @@ class UserController {
       user: ['id', 'fName', 'lName', 'email', 'username'],
       role: ['id', 'title']
     };
-    const query = {
+    let query;
+    query = {
       attributes: userDetails.user,
       include: [
         {
@@ -137,12 +138,16 @@ class UserController {
         }
       ]
     };
+    query = { attributes: userDetails.user };
+    query.limit = req.query.limit || null;
+    query.offset = req.query.offset || null;
+    query.order = [['createdAt', 'DESC']];
     db.User
-      .findAll(query)
+      .findAll({ query, limit: query.limit, offset: query.offset })
       .then((allUsers) => {
         if (allUsers)
           res.status(200).send({
-            message: "Listing available users",
+            message: 'Listing available users',
             data: allUsers
           });
       })
