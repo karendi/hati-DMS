273bdbf0a179028173f48326c6d5ff4705aec132
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-21 17:33:43 +0300

    [Chore #141904245] Lint file

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+/* eslint no-console: 0 */
 import dotenv from 'dotenv';
-import app from './server/routes/Index';
+import app from './server/routes/index';
 
 dotenv.config({ silent: true });
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, (error) => {
-	if (!error) {
-		console.log(`App listening on port ${port}...`);
-	} else {
-		console.log('error');
-	}
+  if (error) {
+    console.log('error');
+  }
+  console.log(`App listening on port ${port}...`);
 });
