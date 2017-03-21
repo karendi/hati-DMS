184ef67fb5476f18991846f4ced73b6b5bf8af4e
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-21 17:36:18 +0300

    [Chore #141904245] Add code comments

--- server/controllers/RoleController.js
+++ server/controllers/RoleController.js
@@ -1,13 +1,28 @@
-import db from '../models/Index.js';
+import db from '../models/index';
 
+/**
+ * Role Controller
+ *
+ * Creates the RoleController
+ * @class
+ */
 class RoleController {
+  /**
+   * createRole
+   *
+   * Creates a new role
+   * @param {object} req The request object
+   * @param {object} res The response object
+   * @returns {void}
+   */
   static createRole(req, res) {
     let roleInfo = {};
     if (req.body.id) {
       roleInfo = { title: req.body.title, id: req.body.id };
     } else { roleInfo = { title: req.body.title }; }
-    if (!req.body.title)
+    if (!req.body.title) {
       return res.status(400).send({ message: 'Title cannot be blank' });
+    }
     const title = req.body.title;
     db.Role
       .findOne({
@@ -20,26 +35,36 @@ class RoleController {
             message: 'Role already exists'
           });
         }
-      return db.Role
-        .create(roleInfo)
-        .then((role) => {
-          res.status(200).send({ message: 'The role was created successfully', role });
-        })
-        .catch((err) => {
-          res.status(400).send({ message: 'There was a problem creating the role', err })
-        });
-    });
+        return db.Role
+          .create(roleInfo)
+          .then((role) => {
+            res.status(200).send({ message: 'The role was created successfully', role });
+          })
+          .catch((err) => {
+            res.status(400).send({ message: 'There was a problem creating the role', err });
+          });
+      });
   }
 
+  /**
+   * updateRole
+   *
+   * Updates an existing role
+   * @param {object} req The request object
+   * @param {object} res The response object
+   * @returns {void}
+   */
   static updateRole(req, res) {
     db.Role
       .findById(req.params.id)
       .then((role) => {
-        if (!role)
+        if (!role) {
           return res.status(404).send({ message: 'Role was not found' });
+        }
         const title = req.body.title;
-        if (!title)
+        if (!title) {
           return res.status(404).send({ message: 'Title cannot be empty' });
+        }
         role.update({
           title: req.body.title || role.title
         })
@@ -51,12 +76,21 @@ class RoleController {
       });
   }
 
+  /**
+   * deleteRole
+   *
+   * Deletes a specific role
+   * @param {object} req The request object
+   * @param {object} res The response object
+   * @returns {void}
+   */
   static deleteRole(req, res) {
     db.Role
       .findById(req.params.id)
       .then((role) => {
-        if (!role)
+        if (!role) {
           return res.status(400).send({ message: 'Role was not found' });
+        }
         role.destroy()
         .then(() => {
           res.status(200).send({ message: 'Role was successfully deleted' });
@@ -64,22 +98,40 @@ class RoleController {
       });
   }
 
+  /**
+   * listAllRoles
+   *
+   * Lists public roles
+   * @param {object} req The request object
+   * @param {object} res The response object
+   * @returns {void}
+   */
   static listAllRoles(req, res) {
     db.Role
       .findAll()
       .then((allRoles) => {
-        if (!allRoles)
+        if (!allRoles) {
           return res.status(404).send({ message: 'A problem was encountered while getting roles' });
+        }
         res.status(200).send({ message: 'Listing available roles', data: allRoles });
       });
   }
 
+  /**
+   * getSpecificRole
+   *
+   * Gets a specific role
+   * @param {object} req The request object
+   * @param {object} res The response object
+   * @returns {void}
+   */
   static getSpecificRole(req, res) {
     db.Role
       .findById(req.params.id)
       .then((role) => {
-        if (!role)
+        if (!role) {
           return res.status(404).send({ message: 'Role was not found' });
+        }
         res.status(200).send({ message: 'Role found!', data: role });
       });
   }
