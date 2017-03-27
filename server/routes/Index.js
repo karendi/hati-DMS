a3f58497690782f8937c86099fb42206188402e2
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-27 16:04:56 +0300

    [Feature #140614543] Remove default index response

--- server/routes/Index.js
+++ server/routes/Index.js
@@ -13,10 +13,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.status(200).send({ message: 'Welcome to Hati!' });
-});
-
 app.use('/api/users', userRouter);
 app.use('/api/documents', documentRouter);
 app.use('/api/roles', roleRouter);
