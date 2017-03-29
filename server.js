4e1bfdde9c6ea644f2968ef5fdccdd4e645dd5bb
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-29 21:48:53 +0300

    [Chore #142209709] Import path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint no-console: 0 */
 require('dotenv').config({ silent: true });
 const app = require('./server/routes/Index.js');
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 
