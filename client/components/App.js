58847041186c78831dd543e73f9f42b8d8d47b7b
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-29 11:02:23 +0300

    [Feature #142530227] Remove container-fluid class

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,12 @@
 // This component handles the App template
 import React, { PropTypes } from 'react';
 import Header from './common/header';
+
 class App extends React.Component {
+
   render() {
     return (
-      <div className="container-fluid">
+      <div>
         <Header />
         {this.props.children}
       </div>
