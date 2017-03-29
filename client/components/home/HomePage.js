d7110dbe3f6cb4b137e5508ade6b9706b33e4acc
Jackie W. Macharia <jackline.macharia@andela.com>
2017-03-29 11:06:32 +0300

    [Feature #142530227] Style homepage component

--- client/components/home/HomePage.js
+++ client/components/home/HomePage.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router';
 class HomePage extends React.Component {
   render() {
     return (
-      <div className="jumbotron text-center">
-        <h1>Document Management</h1>
-        <p>React front end rendering</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+      <div className="text-center">
+        <center>
+          <h1>Hati DMS</h1>
+          <p>Welcome to Hati!</p>
+          <Link to="about" className="waves-effect waves-light btn btn-custom">Learn More</Link>
+        </center>
       </div>
     );
   }
