c9cadaf803deaa0f91ceb7067e2a1a9e1fd45205
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-03 16:24:02 +0300

    [Chore #142209709] Fix staging Heroku deployment

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/index.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/index.html'));
-})
-
 app.listen(port, (error) => {
   if (error) {
     console.log(error);
