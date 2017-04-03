d6c6f567182befd5b09374e9318108718d63c292
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-03 21:11:37 +0300

    [Feature #142530227] Rename webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  // debug: true,
+  // devtool: 'inline-source-map',
+  // noInfo: false,
+  entry: [
+    'eventsource-polyfill',
+    'webpack-hot-middleware/client?reload=true',
+    path.resolve(__dirname, 'client/index')
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'client')
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, include: path.join(__dirname, 'client'), loader: ['babel-loader'] },
+      { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" }
+    ]
+  },
+  watch: true
+};
