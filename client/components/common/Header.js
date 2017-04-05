24a540e6be874f0aa4bb1c156efbc0349ee28bf1
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-05 11:17:14 +0300

    [Feature #142530227] Troubleshoot menu items authenticated display

--- client/components/common/Header.js
+++ client/components/common/Header.js
@@ -1,7 +1,9 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import { logout } from '../../actions/authActions';
+import '../../utils/checkAuth';
 
-const Header = () => {
+const Header = ({ isAuthenticated, logout }) => {
   return (
     <nav className="navbar nav-extended">
       <div className="navbar-wrapper">
@@ -14,11 +16,15 @@ const Header = () => {
         {' | '}
         <Link to="/about" activeClassName="active">About</Link>
         <div className="nav-edge">
-          <Link to="/profile" activeClassName="active">Profile</Link>
-          {' | '}
-          <Link to="/signup" activeClassName="active">Sign up</Link>
-          {' | '}
-          <Link to="/login" activeClassName="active">Login</Link>
+          {isAuthenticated ? (
+            <div>
+              <Link to="/profile" activeClassName="active">Profile | </Link>
+              <Link to="/" onClick={() => logout()}>Logout</Link> 
+            </div>) : (
+            <div>
+              <Link to="/signup" activeClassName="active">Sign up | </Link>
+              <Link to="/login" activeClassName="active">Login</Link> 
+            </div>) }
         </div>
       </div>
     </nav>
