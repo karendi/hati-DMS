4ee1ce6ad14d93b216bdfa086d4e7497725424a8
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-05 11:22:40 +0300

    [Feature #142530227] Add action types

---
+++ client/actions/types.js
@@ -0,0 +1,7 @@
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const SIGNUP_USER = 'SIGNUP_USER';
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const IS_AUTHENTICATED = 'IS_AUTHENTICATED';
