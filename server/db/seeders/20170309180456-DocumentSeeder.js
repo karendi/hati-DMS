958243a6cb6bb9569d7f5ba567afa97343a8e90c
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-07 12:15:48 +0300

    [Chore #143304217] Add unique title/content doc

--- server/db/seeders/20170309180456-DocumentSeeder.js
+++ server/db/seeders/20170309180456-DocumentSeeder.js
@@ -108,6 +108,15 @@ module.exports = {
         userRoleId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        title: 'Ever heard of Nyumbani',
+        content: 'It is similar to Maskani',
+        access: 'public',
+        userId: 4,
+        userRoleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], {});
   },
