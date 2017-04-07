4b85caba5371205455a42bfdb079770de60cac40
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-07 10:03:24 +0300

    [Feature #143117755] Set auth reducers

--- client/reducers/authReducers.js
+++ client/reducers/authReducers.js
@@ -1,18 +1,58 @@
 import isEmpty from 'lodash/isEmpty';
 import * as types from '../actions/types';
+import * as tokenUtils from '../utils/tokenUtils';
 
-const initialState = {
-  isAuthenticated: false,
-  user: {}
-};
-
-export default (state = initialState, action = {}) => {
+const authReducer = (state = { isFetching: false, isAuthenticated: !!tokenUtils.getAuthToken() }, action) => {
   switch (action.type) {
     case types.SET_CURRENT_USER:
       return {
         isAuthenticated: !isEmpty(action.user),
         user: action.user
       };
-    default: return state;
+    case types.LOGIN_USER:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isAuthenticated: false,
+      });
+    case types.LOGIN_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: true,
+        errorMessage: '',
+        user: action.user.user
+      });
+    case types.LOGIN_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: false,
+        errorMessage: action.message.message
+      });
+    case types.LOGOUT_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isAuthenticated: false,
+        user: null
+      });
+    case types.SIGNUP_USER:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isSignedUp: false,
+      });
+    case types.SIGNUP_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isSignedUp: true,
+        errorMessage: '',
+      });
+    case types.SIGNUP_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isSignedUp: false,
+        errorMessage: 'Server Error.'
+      });
+    default:
+      return state;
   }
 };
+
+export default authReducer;
