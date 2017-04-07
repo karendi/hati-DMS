bae3451cbd6ef1d74c83e1abd37e64b6325f4ca8
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-07 12:16:30 +0300

    [Chore #143304217] Add documents association

--- server/models/User.js
+++ server/models/User.js
@@ -46,7 +46,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     classMethods: {
       associate: (models) => {
-        User.hasMany(models.Document, { foreignKey: 'userId' });
+        User.hasMany(models.Document, { foreignKey: 'userId', as: 'documents' });
         User.belongsTo(models.Role, {
           foreignKey: 'roleId',
           onDelete: 'CASCADE'
