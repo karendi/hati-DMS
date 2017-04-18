ea3a2e78535168c5381f6c5c933469d2092d5793
Jackie W. Macharia <jackline.macharia@andela.com>
2017-04-18 11:44:48 +0300

    [Chore #143313979] Add edit and delete options to owner documents

--- client/components/documents/DocumentView.js
+++ client/components/documents/DocumentView.js
@@ -2,12 +2,15 @@ import React, { PropTypes } from 'react';
 import Chip from 'material-ui/Chip';
 import Gravatar from 'react-gravatar';
 import md5 from 'blueimp-md5';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import Header from '../common/Header';
-
+import { getUserFromToken } from '../../utils/tokenUtils';
 const styles = {
   titleStyle: {
     color: '#ffffff'
@@ -21,6 +24,26 @@ const DocumentView = props => (
   <div>
     <GridList
       cellHeight={180} cols={1}>
+      { getUserFromToken().userId === props.document.userId ?
+        <IconMenu
+          style={{ float: 'right' }}
+          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        >
+          <MenuItem primaryText="Edit Document" onTouchTap={() =>
+            props.onUpdate(props.document)} />
+          <MenuItem
+            primaryText="Delete Document" onTouchTap={() => {
+            props.deleteDocument(props.document.id)
+              .then(() => {
+                props.listDocuments();
+              });
+          }
+          }
+          />
+        </IconMenu> : <span />
+      }
         <GridTile
           title={`Title: ${props.document.title}`}
           titleStyle={styles.titleStyle}
